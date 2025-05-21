1d5fb7fc6d8be42eee6ad8b14145d1502f36865a
Ido Tausi <ido.tausi@gmail.com>
2025-05-21 12:01:35 +0300

    Added RawFileHandler template

---
+++ CriticalFuse/FileHandlers/RawFile.h
@@ -0,0 +1,14 @@
+#ifndef RAW_FILE_HANDLERS_HPP
+#define RAW_FILE_HANDLERS_HPP
+
+#include "AbstractFile.h"
+
+class RawFileHandler : public AbstractFileHandler {
+public:
+       
+private:
+       
+};
+
+
+#endif // RAW_FILE_HANDLERS_HPP
